8eedf130b678a8c5568f87a6c7045d225c35aa56
James Berkheimer <5049386+james-berkheimer@users.noreply.github.com>
2022-12-18 23:13:56 -0800

    Update main.py

--- src/filetools/main.py
+++ src/filetools/main.py
@@ -17,40 +17,42 @@ import modules.naming_files as naming_files
 import modules.utils as utils
 
 # --------------------------------------------------------------------------------
-# Globals
-# --------------------------------------------------------------------------------
-utils.make_shows_map() #Make and/or update the shows_map.ini
-CONFIG = utils.get_config()
-FILE_ROOT = Path(CONFIG['paths']['FILE_ROOT'])
-
-# --------------------------------------------------------------------------------
 # Main
 # --------------------------------------------------------------------------------
 def main():
     ''' Main function
     This is a starting point of the application execution.
     '''
+    # make/Update show_map
+    utils.make_shows_map()
     
     # Get tool arguments
     parsed_args = args.parsed_args
-    print(f"Path to work on: {parsed_args.Path}")
-    if parsed_args.cmd1:
-        for action in parsed_args.cmd1:
-            print(f"{action}")      
-
-    if parsed_args.cmd2:          
-        for action in parsed_args.cmd2:
+    root_dir = Path(parsed_args.root)
+    print(f"Path to work on: {root_dir}")
+    if parsed_args.extractfiles:
+        for action in parsed_args.extractfiles:
             print(f"{action}")   
+            moving_files.extract_files(root_dir)   
 
-    if parsed_args.cmd3:
-        for action in parsed_args.cmd3:
+    if parsed_args.renamefiles:          
+        for action in parsed_args.renamefiles:
+            print(f"{action}")
+            naming_files.rename_files(root_dir)   
+
+    if parsed_args.movefiles:
+        for action in parsed_args.movefiles:
             print(f"{action}")        
+            moving_files.move_files(root_dir)
 
-    if parsed_args.cmd4:
-        for action in parsed_args.cmd4:
-            print(f"{action}")        
+    if parsed_args.deleteemptydirs:
+        for action in parsed_args.deleteemptydirs:
+            print(f"{action}") 
+            
+    if parsed_args.makeconfig:
+        utils.make_config() 
 
-
+# Main
 if __name__ == '__main__':
     return_code = main()
     sys.exit(return_code)
