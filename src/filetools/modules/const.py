dff414f2d2416bea5bfcc718989912e821ef54e6
Berkyjay <james.berkheimer@gmail.com>
2023-03-30 19:17:26 -0700

    ...

--- src/filetools/modules/const.py
+++ src/filetools/modules/const.py
@@ -21,7 +21,7 @@ CURRENT_WORKING_DIR = Path.cwd()
 FILES_TO_DELETE = [".DS_Store","RARBG.txt",".srt",".nfo",".sfv","Thumbs.db",
                    ".txt","Sample.mkv", "Trailer.mkv",".jpg", ".vtx"]
 VIDEO_FILE_EXTENSIONS = [".mkv",".mp4",".mpeg",".mpg",".mov",".avi"]
-FILE_EXCLUDES = [".part", "Sample.mkv", "sample.mkv", "Trailer.mkv"]
+FILE_EXCLUDES = [".part", "Sample.mkv", "sample.mkv", "Trailer.mkv", ".DS_Store"]
 DOC_SHOWS = ["Horizon", "Frontline", "American Experience", "NOVA"]
 
 try:
